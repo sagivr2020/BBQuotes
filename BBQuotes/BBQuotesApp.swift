fb78532daf21a7551a9eeae97ebc211731c3bee9
sagiv <sagivram15193@gmail.com>
2024-09-01 16:18:14 +0300

    Initial Commit

---
+++ BBQuotes/BBQuotesApp.swift
@@ -0,0 +1,17 @@
+//
+//  BBQuotesApp.swift
+//  BBQuotes
+//
+//  Created by sagiv on 01/09/2024.
+//
+
+import SwiftUI
+
+@main
+struct BBQuotesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
